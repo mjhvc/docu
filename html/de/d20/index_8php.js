b0f25e3001b1d83fd48ceaba93992e9d5ff7ccb7
mjhvc <marcvancraesbeeck@scarlet.be>
2014-09-13 12:13:50 +0200

    suppression sortie html

--- html/de/d20/index_8php.js
+++
@@ -1,7 +0,0 @@
-var index_8php =
-[
-    [ "$frontal", "de/d20/index_8php.html#a000c80430e7828b390099c1220bde951", null ],
-    [ "else", "de/d20/index_8php.html#ae39f9db1248f7d3a60c70e1fdfaf0417", null ],
-    [ "if", "de/d20/index_8php.html#af2f31b588662066b78e770fccdf4f5ef", null ],
-    [ "try", "de/d20/index_8php.html#abe4cc9788f52e49485473dc699537388", null ]
-];
